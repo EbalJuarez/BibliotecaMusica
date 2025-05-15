0324507d5f1e0c16244ea16eb234ff03ce67e9aa
Ebal Juárez <ebaljuarez_7@umes.edu.gt>
2025-05-15 08:50:19 -0600

    Agregar archivos de proyecto.

---
+++ Clases/Album.cs
@@ -0,0 +1,11 @@
+﻿namespace BibliotecaMusica.Clases
+{
+    public class Album
+    {
+        public string Titulo {  get; set; }
+        public string Artista { get; set; }
+
+        public List<Cancion> Listado_Canciones = new List<Cancion>(); 
+        public DateTime Fecha_Publicacion { get; set; }
+    }
+}
---
+++ Clases/Cancion.cs
@@ -0,0 +1,9 @@
+﻿namespace BibliotecaMusica.Clases
+{
+    public class Cancion
+    {
+        public string Nombre_Cancion { get; set; }
+        public string Artista {  get; set; }
+        public string Duracion { get; set; }
+    }
+}
