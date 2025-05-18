90457dc0dddd0dfddb30b2b38d727ddb4cdd3626
EbalJuarez <ebaljuarez_7@umes.edu.gt>
2025-05-17 22:43:50 -0600

    Se hicieron cambios en las clases y se completo para el guardado de canciones

--- Clases/Album.cs
+++ Clases/Album.cs
@@ -1,11 +1,16 @@
-﻿namespace BibliotecaMusica.Clases
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaMusica.Clases
 {
     public class Album
     {
+        [Required(ErrorMessage = "El titulo del album es obligatorio.")]
         public string? Titulo {  get; set; }
+        [Required(ErrorMessage = "El nombre del artista es obligatorio.")]
         public string? Artista { get; set; }
 
-        public List<Cancion> Listado_Canciones = new List<Cancion>(); 
+        public List<Cancion> Listado_Canciones { get; set; } = new List<Cancion>();
+        [Required(ErrorMessage = "La fecha es obligatoria.")]
         public DateTime? Fecha_Publicacion { get; set; }
     }
 }
--- Clases/Cancion.cs
+++ Clases/Cancion.cs
@@ -1,9 +1,16 @@
-﻿namespace BibliotecaMusica.Clases
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaMusica.Clases
 {
     public class Cancion
     {
+        [Required(ErrorMessage = "El nombre de la canción es obligatorio.")]
         public string? Nombre_Cancion { get; set; }
-        public string? Artista {  get; set; }
+
+        [Required(ErrorMessage = "La duración es obligatoria.")]
         public string? Duracion { get; set; }
+
+        [Required(ErrorMessage = "El artista es obligatorio.")]
+        public string? Artista { get; set; }
     }
 }
